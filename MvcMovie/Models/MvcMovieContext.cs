c88f98264e34bb537320767958ad6d480251a56f
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-09-03 16:38:38 +0200

    1st commit

---
+++ MvcMovie/Models/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore; 
+
+namespace MvcMovie.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+
+
+    }
+}
